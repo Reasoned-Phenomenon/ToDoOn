9707ec8272047321af801d7c59c9ee0534e394ad
RP <rp177@naver.com>
2021-09-07 12:14:15 +0900

    0906 인사말 교체

--- ToDoOn/src/co/yedam/on/AppMain.java
+++ ToDoOn/src/co/yedam/on/AppMain.java
@@ -23,7 +23,7 @@ public class AppMain implements MENU{
 	
 	//시작 메서드
 	public void start() throws Exception{
-		System.out.println("오늘 할 것은 ON을 할 것. ToDoOn입니다.");
+		System.out.println("오늘 할 것은 ON을 하기. ToDoOn입니다.");
 		System.out.println();
 		System.out.println("===============남아있는 인덱스==============");
 		tdo.showAll();
